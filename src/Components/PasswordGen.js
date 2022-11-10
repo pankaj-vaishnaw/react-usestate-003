3729baa97b310a02527b4c245b353471a2057268
Pankaj Vaishnav <97958495+pankaj-vaishnaw@users.noreply.github.com>
2022-11-10 16:37:29 +0530

    Update PasswordGen.js

--- src/Components/PasswordGen.js
+++ src/Components/PasswordGen.js
@@ -1,79 +1,3 @@
-// import React,{useState} from "react";
-
-// function PasswordGen() {
-
-//   const[password, setPassword] = useState("");
-
-//     let p =0;
-//   const passwordGenerator = () => {
-//     if(p<=0){
-//       return;
-//     }
-//     const upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//     const lowerCaseChar = "abcdefghijklmnopqrstuvwxyz";
-//     const numbers = "0123456789";
-//     const symbols = "!@#$*";
-//     let final = upperCaseChar + lowerCaseChar + numbers + symbols;
-//     let pass = "";
-//     for (let i = 0; i < p; i++) {
-//       let char = Math.floor(Math.random() * final.length + 1);
-//       console.log(char);
-//       pass += final.charAt(char);
-//     }
-//     setPassword(pass);
-//     const upperCase = document.getElementById('uppercase');
-//     const lowerCase = document.getElementById('lowercase');
-//     const number = document.getElementById('number');
-//     const sym = document.getElementById('symbol');
-//     const hasUpperCase = upperCase.checked
-//     const hasLowerCase = lowerCase.checked
-//     const hasNumber = number.checked
-//     const hasSymbol = sym.checked
-//     console.log(hasUpperCase,hasLowerCase,hasNumber,hasSymbol)
-//   };
-
-//   return (
-//     <div className="password flex">
-//       <h1>Password Generator</h1>
-//       <div>
-//         <input className="input" type={"text"} disabled value={password} />
-//         <br />
-//         <div className="length">
-//           <p>Select password length</p>
-//           <select className="select" onChange={(e)=>{p=e.target.value}}>
-//             <option className="option" value={"0"}>0</option>
-//             <option className="option"  value={"1"}>1</option>
-//             <option className="option"  value={"2"}>2</option>
-//             <option className="option"  value={"3"}>3</option>
-//             <option className="option"  value={"4"}>4</option>
-//             <option className="option"  value={"5"}>5</option>
-//             <option className="option"  value={"6"}>6</option>
-//             <option className="option"  value={"7"}>7</option>
-//             <option className="option"  value={"8"}>8</option>
-//           </select>
-//         </div>
-//         <br />
-//         <div>
-//         <input type={"checkbox"} id="uppercase" />
-//         <label htmlFor="uppercase">Include uppercase letters</label>
-//         <br />
-//         <input type={"checkbox"} id="lowercase" />
-//         <label htmlFor="lowercase">Include lowercase letters</label>
-//         <br />
-//         <input type={"checkbox"} id="number" />
-//         <label htmlFor="number">Include numbers</label>
-//         <br />
-//         <input type={"checkbox"} id="symbol" />
-//         <label htmlFor="symbol">Include symbols</label>
-//         </div>
-//         <br />
-//         <button className="btn" onClick={passwordGenerator}>Generate Password</button>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default PasswordGen;
 
 import {useState} from "react";
 import {CopyToClipboard} from 'react-copy-to-clipboard';
@@ -85,8 +9,8 @@ const [output,setOutput ] = useState("");
     var lengthVar = 0;
     let checkUpper =false
     let checkLower =false
-    let checkmiddle = false
-    let checkLast =false
+    let checkNumber = false
+    let checkSymbol =false
 
 
 
@@ -94,8 +18,8 @@ const [output,setOutput ] = useState("");
   
     const upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const lowerCaseChar = "abcdefghijklmnopqrstuvwxyz";
-    const numbers = "0123456789";
-    const symbols = "!@#$%&*";
+    const number = "0123456789";
+    const symbol = "!@#$%&*";
     let finalChar ="";
     let length = lengthVar;
     let password = "";
@@ -106,11 +30,11 @@ const [output,setOutput ] = useState("");
     if (checkLower === true){
         finalChar=finalChar+lowerCaseChar
     }
-    if (checkmiddle === true){
-        finalChar=finalChar+numbers
+    if (checkNumber=== true){
+        finalChar=finalChar+number
     }
-    if (checkLast === true){
-        finalChar=finalChar+symbols
+    if (checkSymbol === true){
+        finalChar=finalChar+symbol
     }
 
     for (let i = 0; i < length; i++) {

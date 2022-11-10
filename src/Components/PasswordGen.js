cc943f3e5706153cc60af7abd908d89b120a8422
Pankaj Vaishnav <97958495+pankaj-vaishnaw@users.noreply.github.com>
2022-11-10 16:43:11 +0530

    Update PasswordGen.js

--- src/Components/PasswordGen.js
+++ src/Components/PasswordGen.js
@@ -61,13 +61,14 @@ const [output,setOutput ] = useState("");
       <div className="passlength">
       <p>Select Password length</p>
       <select className="option"onChange={(e)=>{lengthVar=e.target.value}}>
-        <option value="0">0</option>
-        <option value="1">1</option>
+        
         <option value="2">2</option>
         <option value="3">3</option>
         <option value="4">4</option>
         <option value="5">5</option>
         <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
       </select>
       </div>
       <br />
